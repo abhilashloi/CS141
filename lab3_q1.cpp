27245148e8bf4b6df108b0f022599ecb489126f2
abhilashloi <42176114+abhilashloi@users.noreply.github.com>
2018-08-16 20:16:19 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,21 @@
+// Library
+#include <iostream>
+using namespace std;
+
+int main(){
+        cout << "Hello" << endl;
+        // Declaring variables of all types
+        int value;
+        int a,b; // Declaring variables together also possible
+        double doubleVar = 1.2; // Defining the variables
+        bool boolVar = true; // Declaring the variables
+        char c;//Declaring a variables
+        float d = 18976; // Defining the variables
+        
+        // Displaying the sizes of data types.
+        cout << "Size of a = " << sizeof(a) << endl;
+        cout << "size of doubleVar = " << sizeof (doubleVar) << endl;
+        cout << "size of boolVar = " << sizeof(boolVar) << endl;
+        cout << "size of char = " << sizeof(char) << endl;
+        cout << "size of d = " << sizeof(d) << endl;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,19 @@
+//Library
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,sum,diff,product,quotient,remainder; // Declaring multiple variables
+a=30; //Defining the variable
+b=15; // Defining the variable
+sum=a+b;
+diff=a-b;
+product=a*b;
+quotient=a/b;
+remainder=a%b;  //using arithmatic operators
+cout << "The sum of two numberv is" << sum << endl;
+cout << "The difference of two number is" << diff << endl;
+cout << "The product of two number is" << product << endl;
+cout << "The quotient is" << quotient << endl;
+cout << "The remainder is" << remainder << endl;  // Printing out the results
+}
